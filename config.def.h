55b91abc48848209d29952cde2298acd33efa9bc
Najib Mouqtadi <najib@mouqtadi.com>
2022-02-01 21:25:53 +0100

    [patch] cfacts-vanitygaps

--- config.def.h
+++ config.def.h
@@ -3,6 +3,12 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static       int enablegaps         = 1;
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int attachmode         = 2;        /* 0 master (default), 1 = above, 2 = aside, 3 = below, 4 = bottom */
@@ -48,11 +54,25 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 static const int decorhints  = 1;    /* 1 means respect decoration hints */
 
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
+	{ "D[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ NULL,       NULL },
 };
 
 /* key definitions */
@@ -86,6 +106,25 @@ static Key keys[] = {
 	{ M,          XK_d,      incnmaster,     {.i = -1 } },
 	{ M,          XK_h,      setmfact,       {.f = -0.05} },
 	{ M,          XK_l,      setmfact,       {.f = +0.05} },
+	{ M|S,        XK_h,      setcfact,       {.f = +0.25} },
+	{ M|S,        XK_l,      setcfact,       {.f = -0.25} },
+	{ M|S,        XK_o,      setcfact,       {.f =  0.00} },
+	{ M|A,        XK_u,      incrgaps,       {.i = +1 } },
+	{ M|A|S,      XK_u,      incrgaps,       {.i = -1 } },
+	{ M|A,        XK_i,      incrigaps,      {.i = +1 } },
+	{ M|A|S,      XK_i,      incrigaps,      {.i = -1 } },
+	{ M|A,        XK_o,      incrogaps,      {.i = +1 } },
+	{ M|A|S,      XK_o,      incrogaps,      {.i = -1 } },
+	{ M|A,        XK_6,      incrihgaps,     {.i = +1 } },
+	{ M|A|S,      XK_6,      incrihgaps,     {.i = -1 } },
+	{ M|A,        XK_7,      incrivgaps,     {.i = +1 } },
+	{ M|A|S,      XK_7,      incrivgaps,     {.i = -1 } },
+	{ M|A,        XK_8,      incrohgaps,     {.i = +1 } },
+	{ M|A|S,      XK_8,      incrohgaps,     {.i = -1 } },
+	{ M|A,        XK_9,      incrovgaps,     {.i = +1 } },
+	{ M|A|S,      XK_9,      incrovgaps,     {.i = -1 } },
+	{ M|A,        XK_0,      togglegaps,     {0} },
+	{ M|A|S,      XK_0,      defaultgaps,    {0} },
 	{ M,          XK_Return, zoom,           {0} },
 	{ M,          XK_Tab,    view,           {0} },
 	{ M|S,        XK_c,      killclient,     {0} },
