a7b665c9f3fe65647d2dfb041e72a92d765f8a50
Najib Mouqtadi <najib@mouqtadi.com>
2022-02-13 14:02:34 +0100

    [patch] notitle

--- dwm.c
+++ dwm.c
@@ -95,8 +95,8 @@ enum { NetSupported, NetWMName, NetWMState, NetWMStateAbove, NetWMCheck,
        NetClientList, NetClientListStacking, NetDesktopNames,
        NetDesktopViewport, NetNumberOfDesktops, NetCurrentDesktop, NetLast }; /* EWMH atoms */
 enum { WMProtocols, WMDelete, WMState, WMTakeFocus, WMWindowRole, WMLast }; /* default atoms */
-enum { ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
-       ClkClientWin, ClkRootWin, ClkLast }; /* clicks */
+enum { ClkTagBar, ClkLtSymbol, ClkStatusText, ClkClientWin,
+       ClkRootWin, ClkLast }; /* clicks */
 enum {
 	IconsDefault,
 	IconsBullets,
@@ -767,8 +767,7 @@ buttonpress(XEvent *e)
 					s--;
 				}
 			}
-		} else
-			click = ClkWinTitle;
+		}
 	} else if ((c = wintoclient(ev->window))) {
 		focus(c);
 		restack(selmon);
@@ -1507,8 +1506,8 @@ drawbar(Monitor *m)
 	int x, y = barborder, w, tw = 0;
 	int th = bh - barborder * 2;
 	int mw = m->ww - barborder * 2;
-	int boxs = drw->fonts->h / 9;
-	int boxw = drw->fonts->h / 6 + 2;
+	// int boxs = drw->fonts->h / 9;
+	// int boxw = drw->fonts->h / 6 + 2;
 	unsigned int i, occ = 0, urg = 0;
 	char *icon;
 	Client *c;
@@ -1551,15 +1550,8 @@ drawbar(Monitor *m)
 	x = drw_text(drw, x, y, w, th, lrpad / 2, m->ltsymbol, 0);
 
 	if ((w = mw - tw - x) > th) {
-		if (m->sel) {
-			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-			drw_text(drw, x, y, w - 2 * sp, th, lrpad / 2, m->sel->name, 0);
-			if (m->sel->isfloating)
-				drw_rect(drw, x + boxs, y + boxs, boxw, boxw, m->sel->isfixed, 0);
-		} else {
 			drw_setscheme(drw, scheme[SchemeNorm]);
 			drw_rect(drw, x, y, w - 2 * sp, th, 1, 1);
-		}
 	}
 
 	drw_map(drw, m->barwin, 0, 0, m->ww, bh);
@@ -3022,11 +3014,8 @@ propertynotify(XEvent *e)
 			drawbars();
 			break;
 		}
-		if (ev->atom == XA_WM_NAME || ev->atom == netatom[NetWMName]) {
+		if (ev->atom == XA_WM_NAME || ev->atom == netatom[NetWMName])
 			updatetitle(c);
-			if (c == c->mon->sel)
-				drawbar(c->mon);
-		}
 		if (ev->atom == motifatom)
 			updatemotifhints(c);
 	}
